ce1ecf1970b02aba99654afa66b55230c39e9fd8
rubinhozzz <ruben.flex@gmail.com>
2020-02-27 00:22:54 +0100

    fixing paths after move to api/

--- api/app.js
+++ api/app.js
@@ -14,7 +14,7 @@ var app = express();
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
-nunjucks.configure('views', {
+nunjucks.configure(__dirname + '/views', {
   autoescape: true,
   express: app,
   watch: true
