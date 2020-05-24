93196477b3f897ceb2aeea88c0df43df4c5fa803
rubinhozzz <ruben.flex@gmail.com>
2020-05-24 14:09:29 +0200

    player model created

--- api/app.js
+++ api/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-
 app.listen(port, () => console.log('hey!'))
+
+mongoose.connect('mongodb://localhost/aves', {useNewUrlParser: true, useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
 
 module.exports = app;
---
+++ api/models/player.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const playerSchema = new Schema({
+	firstname: { type: String, required: true },
+	lastname: { type: String, required: true },
+i}, {
+	timestamps: true,
+});
+
+const Player = mongoose.model('Player', playerSchema);
+
+module.exports = Player;
