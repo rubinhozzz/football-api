2e55870336c1aa668e44e90b8db8dd150e32a67a
rubinhozzz <ruben.flex@gmail.com>
2020-05-24 14:52:35 +0200

    player router added

--- api/app.js
+++ api/app.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+const playersRouter = require('./routes/players');
+app.use('/players', playersRouter);
 app.listen(port, () => console.log('hey!'))
 
 mongoose.connect('mongodb://localhost/aves', {useNewUrlParser: true, useUnifiedTopology: true});
