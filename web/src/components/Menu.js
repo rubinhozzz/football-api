e25395c6d7bd863912405633437f55680940d10b
rubinhozzz <ruben.flex@gmail.com>
2021-05-14 15:42:43 +0200

    Initial version for main page (no functional)

---
+++ web/src/components/Menu.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Menu() {
+	return (
+		<ul>
+			<li><a href="/">Players</a></li>
+			<li><a href="/">Skills</a></li>
+			<li><a href="/">Matches</a></li>
+			<li><a href="/">Table</a></li>
+		</ul>
+	)
+}
+
+export default Menu
