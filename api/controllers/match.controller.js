4509e8ffca252b205016955ab153076f08964379
rubinhozzz <ruben.flex@gmail.com>
2021-11-30 20:38:50 +0100

    TeamBname by TeamBName

--- api/controllers/match.controller.js
+++ api/controllers/match.controller.js
@@ -14,13 +14,12 @@ class MatchController {
 
 	async create(req, res) {
 		console.log(req.body.teamA);
-		//let teamA = req.body.teamA
 		try {
 			const match = new Match({
 				location: req.body.location,
 				datetime: req.body.datetime,
 				teamAName: req.body.teamAName,
-				teamBname: req.body.teamBName,
+				teamBName: req.body.teamBName,
 				teamA: req.body.teamA,
 				teamB: req.body.teamB
 			});
