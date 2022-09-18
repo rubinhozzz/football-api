4cf71b6c2edd65326082704a142c5d4883e69ac6
rubinhozzz <ruben.flex@gmail.com>
2022-09-18 17:26:44 +0200

    Set up of MongoDB Atlas

--- api/app.js
+++ api/app.js
@@ -1,16 +1,13 @@
 const express = require('express');
+const config = require('./config/db.config');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
 const app = express();
 const port = 8000;
 
-mongoose.connect("mongodb://localhost:27017/aves", {
-	useNewUrlParser: "true",
-})
-mongoose.connection.on("error", err => {
-	console.log("err", err);
-})
+mongoose.connect(config.db, {useNewUrlParser: "true"})
+mongoose.connection.on("error", err => {console.log("err", err);})
 mongoose.connection.on("connected", (err, res) => {
 	console.log("mongoose is connected");
 })
