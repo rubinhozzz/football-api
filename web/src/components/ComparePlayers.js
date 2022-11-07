3deeb69ae17755ffa69b5e3936d197c075e5c8c0
rubinhozzz <ruben.flex@gmail.com>
2022-11-07 01:01:57 +0100

    Update packages for web

--- web/src/components/ComparePlayers.js
+++ web/src/components/ComparePlayers.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, Suspense } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import PlayerSelect from './PlayerSelect';
 import Layout from './layouts/MainLayout';
