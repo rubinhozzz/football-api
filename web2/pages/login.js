34462259540ed5e97d29482adfd13a1f83a1192e
rubinhozzz <ruben.flex@gmail.com>
2023-03-25 15:09:19 +0100

    Tailwindcss added to project

--- web2/pages/login.js
+++ web2/pages/login.js
@@ -1,9 +1,13 @@
-export default function Index() {
+
+export default function Login() {
 	return (
-		<>
+		<div className="bg-black p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+		<h1 className="text-3xl font-bold underline">
+      	Hello world!
+    	</h1>
 		Username:<input name="username" />
 		Password:<input name="password" />
-		<button type="submit">Log in</button>
-		</>
+		<button type="submit" className="bg-red">Log in</button>
+		</div>
 	)
 }
