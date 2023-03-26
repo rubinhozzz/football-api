5f6af1f4222b1495f8a3c8f4e530936eec10ceaa
rubinhozzz <ruben.flex@gmail.com>
2023-03-26 12:53:41 +0200

    Adding defaults and fixing lengths to fields

--- api2/v1/database/models.py
+++ api2/v1/database/models.py
@@ -18,18 +18,18 @@ class User(Base):
     __tablename__ = "user"
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(100))
-    hashed_password: Mapped[str] = mapped_column(String(30))
-    is_active: Mapped[bool]
+    hashed_password: Mapped[str] = mapped_column(String(100))
+    is_active: Mapped[bool] = mapped_column(default=True)
 
 class Player(Base):
     __tablename__ = "player"
     id: Mapped[int] = mapped_column(primary_key=True)
     firstname: Mapped[str] = mapped_column(String(50))
     lastname: Mapped[str] = mapped_column(String(50))
-    email: Mapped[Optional[str]] = mapped_column(String(100))
-    country_code: Mapped[Optional[str]] = mapped_column(String(5))
-    photo: Mapped[Optional[str]] = mapped_column(LargeBinary())
-    is_active: Mapped[bool]
+    telephone: Mapped[Optional[str]] = mapped_column(String(30))
+    country_code: Mapped[Optional[str]] = mapped_column(String(3))
+    photo: Mapped[Optional[bytes]] = mapped_column(LargeBinary())
+    is_active: Mapped[bool] = mapped_column(default=True)
     matches: Mapped[List["Match"]] = relationship(secondary=player_match)
     match_mvp: Mapped["Match"] = relationship(back_populates="mvp")
     match_pichichis: Mapped["Match"] = relationship(back_populates="pichichis")
@@ -37,11 +37,11 @@ class Player(Base):
 class Match(Base):
     __tablename__ = "match"
     id: Mapped[int] = mapped_column(primary_key=True)
-    datetime: Mapped[datetime]
+    datetime: Mapped[datetime] = mapped_column(default=datetime.now())
     teamA_name: Mapped[str] = mapped_column(String(100))
     teamB_name: Mapped[str] = mapped_column(String(100))
-    teamA_score: Mapped[int]
-    teamB_score: Mapped[int]
+    teamA_score: Mapped[Optional[int]] = mapped_column(default=0)
+    teamB_score: Mapped[Optional[int]] = mapped_column(default=0)
     location_id: Mapped[int] = mapped_column(ForeignKey("location.id")) 
     location: Mapped["Location"] = relationship(back_populates="match")
     mvp_id: Mapped[int] = mapped_column(ForeignKey("player.id")) 
@@ -52,8 +52,8 @@ class Location(Base):
     __tablename__ = "location"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(100))
-    postcode: Mapped[Optional[str]] = mapped_column(String(50))
+    postcode: Mapped[Optional[str]] = mapped_column(String(10))
     address: Mapped[Optional[str]] = mapped_column(String(100))
-    is_active: Mapped[bool]
+    is_active: Mapped[bool] = mapped_column(default=True)
     match: Mapped["Match"] = relationship(back_populates="location")
     
