75765ac731ad81eec0c97e0464d52456e1adaefb
rubinhozzz <ruben.flex@gmail.com>
2023-03-29 11:19:59 +0200

    Get first connection with database using asyncio

--- api2/v1/database/database.py
+++ api2/v1/database/database.py
@@ -1,34 +1,15 @@
-import logging
-from typing import AsyncIterator
-#from sqlalchemy import create_engine, MetaData
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import declarative_base
-#from sqlalchemy.orm import sessionmaker
-#mport databases
-
-logger = logging.getLogger(__name__)
 
 SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://rubinhozzz:12345@localhost/aves"
 
-#database = databases.Database(SQLALCHEMY_DATABASE_URL)
-
-#metadata = MetaData()
-
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
-
-#SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-AsyncSessionLocal = async_sessionmaker(
-    bind=engine,
-    autoflush=False,
-    future=True,
-)
+async_session = async_sessionmaker(engine, expire_on_commit=False)
 
-async def get_session() -> AsyncIterator[async_sessionmaker]:
-    try:
-        yield AsyncSessionLocal
-    except SQLAlchemyError as e:
-        logger.exception(e)
+# Dependency
+async def get_session() -> AsyncSession:
+    async with async_session() as session:
+        yield session
--- api2/v1/main.py
+++ api2/v1/main.py
@@ -1,25 +1,24 @@
-from fastapi import Request, FastAPI
+from fastapi import Request, FastAPI, Depends
+from fastapi.responses import JSONResponse
 import database.models as models
-from database.database import engine, Base
+from database.database import engine, Base, get_session
 import asyncio
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 app = FastAPI()
 
-@app.on_event("startup")
-async def startup():
-    #await database.connect()
-    pass
-
-@app.on_event("shutdown")
-async def shutdown():
-    #await database.disconnect()
-    pass
+@app.get("/", include_in_schema=False)
+async def health() -> JSONResponse:
+    return JSONResponse({"message": "It worked!!"})
 
 @app.get("/players/")
-async def get_players():
-    #query = models.Player.select()
-    #return await database.fetch_all(query)
-    pass
+async def get_players(session: AsyncSession = Depends(get_session)) -> JSONResponse:
+    stmt = select(models.Player).order_by(models.Player.id)
+    r = await session.execute(stmt)
+    for row in r.scalars():
+        print(f"{row.id} {row.firstname} {row.lastname}")
+    return JSONResponse({"message": "It worked!!"})
 
 async def async_main():
     async with engine.begin() as conn:
@@ -27,4 +26,8 @@ async def async_main():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
-asyncio.run(async_main())
+#asyncio.run(async_main())
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
