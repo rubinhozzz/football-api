eae8d77877859010f7d199c2f2f37703a092e505
rubinhozzz <ruben.flex@gmail.com>
2023-03-29 14:12:36 +0200

    Add json encode to response

--- api2/v1/database/schemas.py
+++ api2/v1/database/schemas.py
@@ -1,1 +1,7 @@
 from pydantic import BaseModel
+
+class PlayerIn(BaseModel):
+	pass
+
+class Player(BaseModel):
+    pass
--- api2/v1/main.py
+++ api2/v1/main.py
@@ -1,5 +1,6 @@
 from fastapi import Request, FastAPI, Depends
 from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 import database.models as models
 from database.database import engine, Base, get_session
 import asyncio
@@ -12,8 +13,30 @@ app = FastAPI()
 async def health() -> JSONResponse:
     return JSONResponse({"message": "It worked!!"})
 
+@app.get("/players/{id}")
+async def get_players(id: int, session: AsyncSession = Depends(get_session)) -> JSONResponse:
+    stmt = select(models.Player).filter_by(id=id).order_by(models.Player.id)
+    result = await session.execute(stmt)
+    player = result.scalars().first()
+    return jsonable_encoder(player)
+
 @app.get("/players/")
 async def get_players(session: AsyncSession = Depends(get_session)) -> JSONResponse:
+    stmt = select(models.Player).order_by(models.Player.id)
+    result = await session.execute(stmt)
+    players = result.scalars().all()
+    return jsonable_encoder(players)
+
+@app.post("/players/")
+async def create_player(session: AsyncSession = Depends(get_session)) -> JSONResponse:
+    stmt = select(models.Player).order_by(models.Player.id)
+    r = await session.execute(stmt)
+    for row in r.scalars():
+        print(f"{row.id} {row.firstname} {row.lastname}")
+    return JSONResponse({"message": "It worked!!"})
+
+@app.put("/players/{id}")
+async def update_player(session: AsyncSession = Depends(get_session)) -> JSONResponse:
     stmt = select(models.Player).order_by(models.Player.id)
     r = await session.execute(stmt)
     for row in r.scalars():
@@ -27,7 +50,6 @@ async def async_main():
         await conn.run_sync(Base.metadata.create_all)
 
 #asyncio.run(async_main())
-
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
