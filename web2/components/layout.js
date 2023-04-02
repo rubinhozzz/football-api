12a1b3c1a76e8635eeea8d62f17b0eff82ba4ba3
rubinhozzz <ruben.flex@gmail.com>
2023-04-02 23:28:40 +0200

    Add references

--- web2/components/layout.js
+++ web2/components/layout.js
@@ -13,10 +13,10 @@ export default function Layout({ children }) {
 			</div>
 			<div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
 				<div className="text-sm lg:flex-grow">
-				<a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+				<a href="/matches" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
 					Matches
 				</a>
-				<a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+				<a href="/players" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
 					Players
 				</a>
 				</div>
