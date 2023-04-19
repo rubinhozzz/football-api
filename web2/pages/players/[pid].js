32762f7013d00a2519710b497fba91f656179d54
rubinhozzz <ruben.flex@gmail.com>
2023-04-19 12:44:50 +0200

    Add functionality for update player

--- web2/pages/players/[pid].js
+++ web2/pages/players/[pid].js
@@ -9,12 +9,25 @@ export default function UpdatePlayer(props) {
 	const { pid } = router.query
 	const { data, error, isLoading } = useSWR(pid ? `http://192.168.178.44:8000/players/${pid}` : null, fetcher);
 
-	function onSubmit(data) {
-		alert('UPDATE');
+	async function handleSubmit(data) {
+		const options = {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(data)
+		}
+		try{
+			const response = await fetch(`http://192.168.178.44:8000/players/${pid}`, options);
+			const result = await response.json()
+			router.push('/players');
+		} catch (error) {
+			alert(error);
+		}
 	}
 
 	if (error) return <div>Failed to load</div>
 	if (isLoading) return <div>Loading...</div>
 
-	return (<PlayerForm data={data} onSubmit={onSubmit}></PlayerForm>)
+	return (<PlayerForm data={data} onSubmit={handleSubmit}></PlayerForm>)
 }
