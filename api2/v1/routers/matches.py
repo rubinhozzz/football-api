2fa90a14532f138e458d86ff82484b8ba7c0793e
rubinhozzz <ruben.flex@gmail.com>
2023-04-26 12:52:38 +0200

    Remove for matches added

--- api2/v1/routers/matches.py
+++ api2/v1/routers/matches.py
@@ -36,50 +36,54 @@ async def get_player(id: int, session: AsyncSession = Depends(get_session)) -> J
 @router.post('/')
 async def create_match(matchSchema: Match, session: AsyncSession = Depends(get_session)) -> JSONResponse:
 	async with session.begin():
-		stmt = select(models.Location).filter_by(id=matchSchema.location).order_by(models.Location.id)
-		result = await session.execute(stmt)
-		location = result.scalars().one()
 		m = models.Match(
 			datetime=matchSchema.datetime, 
 			teamA_name=matchSchema.teamA_name,
 			teamB_name=matchSchema.teamB_name,
 			teamA_score=matchSchema.teamA_score,
 			teamB_score=matchSchema.teamB_score)
+		# getting location
+		stmt = select(models.Location).filter_by(id=matchSchema.location).order_by(models.Location.id)
+		result = await session.execute(stmt)
+		location = result.scalars().one()
 		m.location = location
+		# getting mvp
+		if matchSchema.mvp:
+			stmt = select(models.Player).filter_by(id=matchSchema.mvp).order_by(models.Player.id)
+			result = await session.execute(stmt)
+			player = result.scalars().one()
+			m.mvp = player
+		if matchSchema.pichichis:
+			pass
 		session.add(m)
 		await session.commit()
 		return jsonable_encoder(m)
 
-"""
 @router.put('/{id}')
-async def update_player(id: int, playerSchema: Player, session: AsyncSession = Depends(get_session)) -> JSONResponse:
-	try:
-		async with session.begin():
-			stmt = select(models.Player).filter_by(id=id).order_by(models.Player.id)
+async def update_match(id: int, matchSchema: Match, session: AsyncSession = Depends(get_session)) -> JSONResponse:
+	async with session.begin():
+		try:
+			stmt = select(models.Match).filter_by(id=id)
 			result = await session.execute(stmt)
-			player = result.scalars().one()
-			player.firstname = playerSchema.firstname
-			player.lastname = playerSchema.lastname
-			player.country_code = playerSchema.country_code
+			match = result.scalars().one()
+			#player.firstname = playerSchema.firstname
+			#player.lastname = playerSchema.lastname
+			#player.country_code = playerSchema.country_code
 			await session.commit()
-			return jsonable_encoder(player)
-	except NoResultFound as ex:
-		return JSONResponse({'ok': False, 'error': str(ex)}, status_code=404)
-	except Exception as ex:
-		print(ex)
-		return JSONResponse({'ok': False, 'error': str(ex)}, status_code=500)   
-"""
+			return jsonable_encoder(match)
+		except NoResultFound as ex:
+			return JSONResponse({'ok': False, 'error': str(ex)}, status_code=404)
 
 @router.delete('/{id}')
-async def delete_player(id: int, session: AsyncSession = Depends(get_session)) -> JSONResponse:
+async def delete_match(id: int, session: AsyncSession = Depends(get_session)) -> JSONResponse:
 	async with session.begin():
-		stmt = select(models.Match).filter_by(id=id)
-		result = await session.execute(stmt)
-		try: 
+		try:
+			stmt = select(models.Match).filter_by(id=id)
+			result = await session.execute(stmt)
 			match = result.scalars().one()
+			await session.delete(match)
+			await session.commit()
+			return JSONResponse({'ok': True})
 		except NoResultFound as ex:
-			return JSONResponse({'ok': False, 'error': str(ex)}, status_code=404)    
-		await session.delete(match)
-		await session.commit()
-		return JSONResponse({'ok': True})
+			return JSONResponse({'ok': False, 'error': str(ex)}, status_code=404)
 		
