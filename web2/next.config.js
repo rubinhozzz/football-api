7198490988945dc3bc46a666d7c9f60446027c13
rubinhozzz <ruben.flex@gmail.com>
2023-05-16 19:42:43 +0200

    Redirect / to /matches

--- web2/next.config.js
+++ web2/next.config.js
@@ -2,4 +2,13 @@ module.exports = {
 	env: {
 		apiRootURI: 'http://192.168.178.44:8000',
 	},
+	async redirects() {
+		return [
+		  {
+			source: '/',
+			destination: '/matches',
+			permanent: true,
+		  },
+		];
+	  },
 }
