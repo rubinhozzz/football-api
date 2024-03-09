4a1722750ef83531cb5328bafe0382b1897eb750
rubinhozzz <ruben.flex@gmail.com>
2024-03-09 22:45:21 +0100

    models changed

--- app/database/models.py
+++ app/database/models.py
@@ -1,29 +1,19 @@
 from typing import List, Optional
 from datetime import datetime
 from sqlalchemy.orm import Mapped, relationship, mapped_column
-from sqlalchemy import Boolean, Column, ForeignKey, String, LargeBinary, Table, DateTime
+from sqlalchemy import Boolean, ForeignKey, String, LargeBinary, DateTime
 from .database import Base
 
-"""
-player_match = Table(
-    "player_match",
-    Base.metadata,
-    Column("player_id", ForeignKey("player.id")),
-    Column("match_id", ForeignKey("match.id")),
-    Column("team", String, default=''),
-    Column("pichichi", Boolean, default=False)
-)
-"""
 
 class PlayerMatch(Base):
-	__tablename__ = 'player_match'
-	player_id: Mapped[int] = mapped_column(ForeignKey("player.id"), primary_key=True) 
-	match_id: Mapped[int] = mapped_column(ForeignKey("match.id"), primary_key=True) 
-	team: Mapped[Optional[str]] = mapped_column(String(1))
-	pichichi: Mapped[bool] = mapped_column(Boolean, server_default='f', default=False)
-        
-	player: Mapped["Player"] = relationship(back_populates="matches", lazy="selectin")
-	match: Mapped["Match"] = relationship(back_populates="players", lazy="selectin")
+    __tablename__ = 'player_match'
+    player_id: Mapped[int] = mapped_column(ForeignKey("player.id"), primary_key=True) 
+    match_id: Mapped[int] = mapped_column(ForeignKey("match.id"), primary_key=True) 
+    team: Mapped[str] = mapped_column(String(1))
+    pichichi: Mapped[bool] = mapped_column(Boolean, server_default='f', default=False)
+    player: Mapped["Player"] = relationship(back_populates="matches", lazy="selectin")
+    match: Mapped["Match"] = relationship(back_populates="players", lazy="selectin")
+
 
 class User(Base):
     __tablename__ = "user"
@@ -32,32 +22,33 @@ class User(Base):
     hashed_password: Mapped[str] = mapped_column(String(100))
     is_active: Mapped[bool] = mapped_column(Boolean, server_default='t', default=True)
 
+
 class Player(Base):
-	__tablename__ = "player"
-	id: Mapped[int] = mapped_column(primary_key=True)
-	firstname: Mapped[str] = mapped_column(String(50))
-	lastname: Mapped[str] = mapped_column(String(50))
-	telephone: Mapped[Optional[str]] = mapped_column(String(30))
-	country_code: Mapped[Optional[str]] = mapped_column(String(3))
-	photo: Mapped[Optional[bytes]] = mapped_column(LargeBinary())
-	is_active: Mapped[bool] = mapped_column(Boolean, server_default='t', default=True)
-	match_mvp: Mapped["Match"] = relationship(back_populates="mvp")
-	matches: Mapped[List["PlayerMatch"]] = relationship(back_populates="player", cascade='save-update, merge, delete, delete-orphan', lazy="selectin")
-    
+    __tablename__ = "player"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    firstname: Mapped[str] = mapped_column(String(50))
+    lastname: Mapped[Optional[str]] = mapped_column(String(50))
+    country_code: Mapped[Optional[str]] = mapped_column(String(3))
+    photo: Mapped[Optional[bytes]] = mapped_column(LargeBinary())
+    is_active: Mapped[bool] = mapped_column(Boolean, server_default='t', default=True)
+    match_mvp: Mapped["Match"] = relationship(back_populates="mvp")
+    matches: Mapped[List["PlayerMatch"]] = relationship(back_populates="player", cascade='save-update, merge, delete, delete-orphan', lazy="selectin")
+
 
 class Match(Base):
-	__tablename__ = "match"
-	id: Mapped[int] = mapped_column(primary_key=True)
-	datetime: Mapped[int] = mapped_column(DateTime(timezone=True), default=datetime.now())
-	teamA_name: Mapped[str] = mapped_column(String(100))
-	teamB_name: Mapped[str] = mapped_column(String(100))
-	teamA_score: Mapped[Optional[int]] = mapped_column(default=0)
-	teamB_score: Mapped[Optional[int]] = mapped_column(default=0)
-	location_id: Mapped[int] = mapped_column(ForeignKey("location.id")) 
-	location: Mapped["Location"] = relationship(back_populates="matches", lazy='selectin')
-	mvp_id: Mapped[Optional[int]] = mapped_column(ForeignKey("player.id")) 
-	mvp: Mapped[Optional["Player"]] = relationship(back_populates="match_mvp")
-	players: Mapped[List["PlayerMatch"]] = relationship(back_populates="match", lazy="selectin", cascade='save-update, merge, delete, delete-orphan')
+    __tablename__ = "match"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    datetime: Mapped[int] = mapped_column(DateTime(timezone=True), default=datetime.now())
+    teamA_name: Mapped[str] = mapped_column(String(100))
+    teamB_name: Mapped[str] = mapped_column(String(100))
+    teamA_score: Mapped[Optional[int]] = mapped_column(default=0)
+    teamB_score: Mapped[Optional[int]] = mapped_column(default=0)
+    location_id: Mapped[int] = mapped_column(ForeignKey("location.id")) 
+    location: Mapped["Location"] = relationship(back_populates="matches", lazy='selectin')
+    mvp_id: Mapped[Optional[int]] = mapped_column(ForeignKey("player.id")) 
+    mvp: Mapped[Optional["Player"]] = relationship(back_populates="match_mvp")
+    players: Mapped[List["PlayerMatch"]] = relationship(back_populates="match", lazy="selectin", cascade='save-update, merge, delete, delete-orphan')
+
 
 class Location(Base):
     __tablename__ = "location"
