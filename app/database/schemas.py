519f245f5c564a3351b5bb792d89dbd6f9f3ae42
rubinhozzz <ruben.flex@gmail.com>
2024-03-09 22:58:40 +0100

    schemas fixed

--- app/database/schemas.py
+++ app/database/schemas.py
@@ -2,27 +2,59 @@ from pydantic import BaseModel
 import datetime
 from typing import Optional
 
-class Location(BaseModel):
+
+class LocationIn(BaseModel):
     name: str
     postcode: Optional[str] = None
     address: Optional[str] = None
-    is_active: Optional[bool] = True
 
-class Player(BaseModel):
-   firstname: str
-   lastname: str
-   country_code: Optional[str] = None
 
-class Match(BaseModel):
-	datetime: datetime.date
-	teamA_name: str
-	teamA_score: int
-	teamA_players: Optional[list[int]] = None
-	teamB_name: str
-	teamB_score: int
-	teamB_players: Optional[list[int]] = None
-	location: Optional[int] = None
-	#location_id: int | None = None
-	mvp: Optional[int] = None
-	#mvp_id: int | None = None
-	pichichis: Optional[list[int]] = None
+class LocationOut(BaseModel):
+    id: int
+    name: str
+    postcode: Optional[str] = None
+    address: Optional[str] = None
+    is_active: bool = True
+
+
+class PlayerIn(BaseModel):
+    firstname: str
+    lastname: Optional[str] = None
+    country_code: Optional[str] = None
+
+
+class PlayerOut(BaseModel):
+    id: int
+    firstname: str
+    lastname: Optional[str] = None
+    country_code: Optional[str] = None
+    active: bool
+
+
+class MatchIn(BaseModel):
+    datetime: datetime.date
+    teamA_name: str
+    teamA_score: int
+    teamA_players: Optional[list[int]] = None
+    teamB_name: str
+    teamB_score: int
+    teamB_players: Optional[list[int]] = None
+    location_id: int
+    mvp_id: Optional[int] = None
+    pichichis: Optional[list[int]] = None
+
+
+class MatchOut(BaseModel):
+    id: int
+    datetime: datetime.date
+    teamA_name: str
+    teamA_score: int
+    teamA_players: Optional[list[int]] = None
+    teamB_name: str
+    teamB_score: int
+    teamB_players: Optional[list[PlayerOut]] = None
+    location: LocationOut
+    location_id: int
+    mvp: Optional[PlayerOut] = None
+    mvp_id: Optional[int]
+    pichichis: Optional[list[PlayerOut]] = None
